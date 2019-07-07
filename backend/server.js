4adcb0998ad9cc69ec2ddb8d7c1a8f0f9d0f0c28
Roushan Sourav <roushansourav@outlook.com>
2019-07-07 19:48:30 +0530

    exercise route complete

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const cors = require('cors');
 const mongoose=require ('mongoose');
+const exercisesRouter=require('./routes/exercises');
+const usersRouter=require('./routes/users');
+
 require('dotenv').config();
 
 const app=express();
@@ -13,7 +16,9 @@ mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true});
 const connection=mongoose.connection;
 connection.once('open',()=>{
    console.log("MongoDB database connection established successfully");
-})
+});
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
 app.listen(port,()=>{
    console.log(`Server is running on port:${port}`);
 
